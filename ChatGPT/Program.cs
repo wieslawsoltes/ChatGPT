813e2c8325ce588d7da3dd53f61f11c449510e9a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-12-06 21:53:25 +0100

    Refactor to SendApiRequestAsync
    Generated by ChatGPT

--- ChatGPT/Program.cs
+++ ChatGPT/Program.cs
@@ -39,6 +39,18 @@ namespace ChatGPT
             // Serialize the request body to JSON using the JsonSerializer.Serialize method overload that takes a JsonSerializerOptions parameter
             var requestBodyJson = JsonSerializer.Serialize(requestBody, serializerOptions);
 
+            // Send the API request and get the response data
+            var responseData = await SendApiRequestAsync(apiUrl, apiKey, requestBodyJson);
+            
+            // Print the response
+            foreach (var choice in responseData.GetProperty("choices").EnumerateArray())
+            {
+                Console.WriteLine(choice.GetProperty("text").GetString());
+            }
+        }
+
+        private static async Task<JsonElement> SendApiRequestAsync(string apiUrl, string apiKey, string requestBodyJson)
+        {
             // Create a new HttpClient for making the API request
             using HttpClient client = new HttpClient();
 
@@ -49,19 +61,13 @@ namespace ChatGPT
             StringContent content = new StringContent(requestBodyJson, Encoding.UTF8, "application/json");
 
             // Send the API request and get the response
-            HttpResponseMessage response = client.PostAsync(apiUrl, content).Result;
+            HttpResponseMessage response = await client.PostAsync(apiUrl, content);
 
             // Deserialize the response
             var responseBody = await response.Content.ReadAsStringAsync();
 
-            // Print the response
-            var responseData = JsonDocument.Parse(responseBody).RootElement;
-
-            // Print the response
-            foreach (var choice in responseData.GetProperty("choices").EnumerateArray())
-            {
-                Console.WriteLine(choice.GetProperty("text").GetString());
-            }
+            // Return the response data
+            return JsonDocument.Parse(responseBody).RootElement;
         }
     }
 }
