53758281bdeab9a9b73b2694e47e5c7da70b9598
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-12-08 23:29:15 +0100

    Use ChatService

--- ChatGPT.UI/MainWindow.axaml.cs
+++ ChatGPT.UI/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace ChatGPT.UI;
 
@@ -7,5 +10,23 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        Submit.Click += SubmitOnClick;
+    }
+
+    private async void SubmitOnClick(object? sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(Prompt.Text))
+        {
+            return;
+        }
+
+        // Get the response data for the prompt
+        var responseData = await ChatService.GetResponseDataAsync(Prompt.Text);
+
+        // Print the response
+        var choices = responseData.Choices;
+
+        Choice.Text = choices?.FirstOrDefault()?.Text;
     }
 }
