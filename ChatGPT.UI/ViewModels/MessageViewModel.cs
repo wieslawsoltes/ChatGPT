c7df0ee82c0f8d2b6cc4dfb9f3849d8120835f17
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-07 13:19:38 +0100

    Update MessageViewModel.cs

--- ChatGPT.UI/ViewModels/MessageViewModel.cs
+++ ChatGPT.UI/ViewModels/MessageViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -50,43 +51,51 @@ public class MessageViewModel : ObservableObject
         });
     }
 
+    [JsonPropertyName("Prompt")]
     public string? Prompt
     {
         get => _prompt;
         set => SetProperty(ref _prompt, value);
     }
 
+    [JsonPropertyName("prompt")]
     public string? Message
     {
         get => _message;
         set => SetProperty(ref _message, value);
     }
 
+    [JsonPropertyName("isSent")]
     public bool IsSent
     {
         get => _isSent;
         set => SetProperty(ref _isSent, value);
     }
 
+    [JsonPropertyName("isAwaiting")]
     public bool IsAwaiting
     {
         get => _isAwaiting;
         set => SetProperty(ref _isAwaiting, value);
     }
 
+    [JsonPropertyName("isError")]
     public bool IsError
     {
         get => _isError;
         set => SetProperty(ref _isError, value);
     }
-    
+
+    [JsonPropertyName("result")]
     public MessageViewModel? Result
     {
         get => _result;
         set => SetProperty(ref _result, value);
     }
 
+    [JsonIgnore]
     public IAsyncRelayCommand SendCommand { get; }
 
+    [JsonIgnore]
     public IRelayCommand EditCommand { get; }
 }
