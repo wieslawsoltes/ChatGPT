82ff0f2950c2924d88a648172f3ed0b9b479646b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-07 13:00:24 +0100

    Refactor into separate views

---
+++ ChatGPT.UI/Views/RequestSettingsView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ChatGPT.UI.Views;
+
+public partial class RequestSettingsView : UserControl
+{
+    public RequestSettingsView()
+    {
+        InitializeComponent();
+    }
+}
