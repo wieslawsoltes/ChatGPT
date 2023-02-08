ad0ac395eea535eda5fef4309398ff31ef27a868
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-08 21:00:24 +0100

    Add MessagesView

---
+++ ChatGPT.UI/Views/MessagesView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ChatGPT.UI.Views;
+
+public partial class MessagesView : UserControl
+{
+    public MessagesView()
+    {
+        InitializeComponent();
+    }
+}
+
