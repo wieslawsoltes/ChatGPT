ad38cc63c9705760539f304914371b0b22ca454f
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-08 14:18:35 +0100

    Add option to toggle transparent window using Ctrl+Shift+A

--- ChatGPT.UI/Views/MainWindow.axaml.cs
+++ ChatGPT.UI/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Platform;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ChatGPT.UI.Views;
 
@@ -7,5 +10,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        KeyBindings.Add(new KeyBinding
+        {
+            Gesture = new KeyGesture(Key.A, KeyModifiers.Control | KeyModifiers.Shift),
+            Command = new RelayCommand(ToggleAcrylicBlur)
+        });
+    }
+
+    private void ToggleAcrylicBlur()
+    {
+        if (TransparencyLevelHint == WindowTransparencyLevel.AcrylicBlur)
+        {
+            SystemDecorations = SystemDecorations.None;
+            ExtendClientAreaChromeHints = ExtendClientAreaChromeHints.NoChrome;
+            TransparencyLevelHint = WindowTransparencyLevel.Transparent;
+            AcrylicBorder.IsVisible = false;
+        }
+        else
+        {
+            SystemDecorations = SystemDecorations.Full;
+            ExtendClientAreaChromeHints = ExtendClientAreaChromeHints.PreferSystemChrome;
+            TransparencyLevelHint = WindowTransparencyLevel.AcrylicBlur;
+            AcrylicBorder.IsVisible = true;
+        }
     }
 }
