0ab1cda8a3c2750e08d12b9aea5eb4074b7de8bc
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-08 14:57:25 +0100

    Update App.axaml.cs

--- ChatGPT.UI/App.axaml.cs
+++ ChatGPT.UI/App.axaml.cs
@@ -1,6 +1,9 @@
+using System;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
 using ChatGPT.UI.Model.Services;
 using ChatGPT.UI.Services;
 using ChatGPT.UI.ViewModels;
@@ -55,5 +58,75 @@ public partial class App : Application
                 .AddTransient<MessageViewModel>()
                 .AddTransient<MainViewModel>()
                 .BuildServiceProvider());
+    }
+
+    public void ToggleAcrylicBlur()
+    {
+        if (Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            if (desktop.MainWindow is MainWindow mainWindow)
+            {
+                if (mainWindow.TransparencyLevelHint == WindowTransparencyLevel.AcrylicBlur)
+                {
+                    mainWindow.SystemDecorations = SystemDecorations.None;
+                    mainWindow.ExtendClientAreaChromeHints = ExtendClientAreaChromeHints.NoChrome;
+                    mainWindow.TransparencyLevelHint = WindowTransparencyLevel.Transparent;
+                    mainWindow.AcrylicBorder.IsVisible = false;
+                }
+                else
+                {
+                    mainWindow.SystemDecorations = SystemDecorations.Full;
+                    mainWindow.ExtendClientAreaChromeHints = ExtendClientAreaChromeHints.PreferSystemChrome;
+                    mainWindow.TransparencyLevelHint = WindowTransparencyLevel.AcrylicBlur;
+                    mainWindow.AcrylicBorder.IsVisible = true;
+                }
+            }
+        }
+    }
+
+    public void ToggleWindowState()
+    {
+        if (Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            if (desktop.MainWindow is MainWindow mainWindow)
+            {
+                if (mainWindow.WindowState == WindowState.Minimized)
+                {
+                    mainWindow.WindowState = WindowState.Normal;
+                    mainWindow.ShowInTaskbar = true;
+                    mainWindow.Show();
+                }
+                else
+                {
+                    mainWindow.WindowState = WindowState.Minimized;
+                    mainWindow.ShowInTaskbar = false;
+                    mainWindow.Hide();
+                }
+            }
+        }
+    }
+
+    private void TrayIcon_OnClicked(object? sender, EventArgs e)
+    {
+        ToggleWindowState();
+    }
+
+    private void ToggleShow_OnClick(object? sender, EventArgs e)
+    {
+        ToggleWindowState();
+    }
+
+    private void ToggleAcrylic_OnClick(object? sender, EventArgs e)
+    {
+        ToggleAcrylicBlur();
+    }
+
+    private void Quit_OnClick(object? sender, EventArgs e)
+    {
+        var app = Ioc.Default.GetService<IApplicationService>();
+        if (app is { })
+        {
+            app.Exit();
+        }
     }
 }
