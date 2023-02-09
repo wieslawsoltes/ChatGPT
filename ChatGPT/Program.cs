03052f40b71a28f86dace045e9bf9b6456bf0df7
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-09 11:42:13 +0100

    Update Program.cs

--- ChatGPT/Program.cs
+++ ChatGPT/Program.cs
@@ -6,8 +6,18 @@ namespace ChatGPT;
 class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
