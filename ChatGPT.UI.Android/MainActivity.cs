c3a5dee2b09420a5088cf065cfc391cc3f8fa384
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-02-14 00:11:28 +0100

    Add Android

---
+++ ChatGPT.UI.Android/MainActivity.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia.Android;
+
+namespace ChatGPT.UI.Android;
+
+[Activity(Label = "ChatGPT", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+public class MainActivity : AvaloniaMainActivity
+{
+}
---
+++ ChatGPT.UI.Android/SplashActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Avalonia.Android;
+
+namespace ChatGPT.UI.Android;
+
+[Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+public class SplashActivity : AvaloniaSplashActivity<App>
+{
+    protected override void OnResume()
+    {
+        base.OnResume();
+
+        StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        Finish();
+    }
+
+    protected override async void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (Avalonia.Application.Current is App app)
+        {
+            try
+            {
+                await app.SaveSettings();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
