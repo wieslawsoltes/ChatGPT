8ab9f156eb63d7068047c1de44652d35f1f79f34
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-05 10:59:52 +0100

    Add iOS

---
+++ ChatGPT.UI.iOS/AppDelegate.cs
@@ -0,0 +1,12 @@
+using Avalonia.iOS;
+using Foundation;
+
+namespace ChatGPT.UI.iOS;
+
+// The UIApplicationDelegate for the application. This class is responsible for launching the 
+// User Interface of the application, as well as listening (and optionally responding) to 
+// application events from iOS.
+[Register("AppDelegate")]
+public class AppDelegate : AvaloniaAppDelegate<App>
+{
+}
