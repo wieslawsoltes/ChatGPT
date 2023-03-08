f017f86ebe115f2da43637dc98570461985156f2
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-08 12:12:44 +0100

    Create GridLengthConverter.cs

---
+++ ChatGPT.UI/Controls/GridLengthConverter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+
+namespace ChatGPT.Controls;
+
+public class GridLengthConverter : IValueConverter
+{
+    public static GridLengthConverter Instance = new();
+    
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string s)
+        {
+            return GridLength.Parse(s);
+        }
+
+        return AvaloniaProperty.UnsetValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is GridLength length)
+        {
+            return length.ToString();
+        }
+
+        return AvaloniaProperty.UnsetValue;
+    }
+}
