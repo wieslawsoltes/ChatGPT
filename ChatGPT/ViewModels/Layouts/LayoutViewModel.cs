2fd6a39bd03cb08092f1fa0208729668a90c466b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-09 21:53:40 +0100

    Update LayoutViewModel.cs

--- ChatGPT/ViewModels/Layouts/LayoutViewModel.cs
+++ ChatGPT/ViewModels/Layouts/LayoutViewModel.cs
@@ -8,6 +8,8 @@ namespace ChatGPT.ViewModels.Layouts;
 [JsonPolymorphic]
 public abstract partial class LayoutViewModel : ObservableObject
 {
+    private double _width;
+    private double _height;
     private bool _showSettings;
     private bool _showChats;
     private bool _showPrompts;
@@ -20,8 +22,22 @@ public abstract partial class LayoutViewModel : ObservableObject
 
         ShowPromptsCommand = new RelayCommand(ShowPromptsAction);
     }
-        
-    [JsonIgnore]
+
+    [JsonPropertyName("width")]
+    public double Width
+    {
+        get => _width;
+        set => SetProperty(ref _width, value);
+    }
+
+    [JsonPropertyName("height")]
+    public double Height
+    {
+        get => _height;
+        set => SetProperty(ref _height, value);
+    }
+
+    [JsonPropertyName("showSettings")]
     public bool ShowSettings
     {
         get => _showSettings;
