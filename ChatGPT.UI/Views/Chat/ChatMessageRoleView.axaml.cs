a0c501f91d829a6222ba5fbf8e38d1e996bcc75b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-15 21:54:45 +0100

    Refactor into user control

---
+++ ChatGPT.UI/Views/Chat/ChatMessageRoleView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ChatGPT.Views.Chat;
+
+public partial class ChatMessageRoleView : UserControl
+{
+    public ChatMessageRoleView()
+    {
+        InitializeComponent();
+    }
+}
