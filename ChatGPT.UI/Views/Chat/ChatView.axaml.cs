f5b4428b2b01436c633ccf3adae54e24b35cef59
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-16 20:46:00 +0100

    Fix scrolling of messages

--- ChatGPT.UI/Views/Chat/ChatView.axaml.cs
+++ ChatGPT.UI/Views/Chat/ChatView.axaml.cs
@@ -1,12 +1,34 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Reactive;
 
 namespace ChatGPT.Views.Chat;
 
 public partial class ChatView : UserControl
 {
+    private bool _scrollToEnd;
+    
     public ChatView()
     {
         InitializeComponent();
+
+        MessagesItemsControl
+            .GetObservable(SelectingItemsControl.SelectedItemProperty)
+            .Subscribe(new AnonymousObserver<object?>(x =>
+            {
+                _scrollToEnd = true;
+            }));
+
+        ChatScrollViewer
+            .GetObservable(ScrollViewer.ExtentProperty)
+            .Subscribe(new AnonymousObserver<Size>(x =>
+            {
+                if (_scrollToEnd)
+                {
+                    ChatScrollViewer.ScrollToEnd();
+                    _scrollToEnd = false;
+                }
+            }));
     }
 }
-
