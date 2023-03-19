9054e7295bdb5777fb5b437ea5e7c7770c57b318
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-19 20:53:13 +0100

    Update main.js

--- ChatGPT.UI.Browser/AppBundle/main.js
+++ ChatGPT.UI.Browser/AppBundle/main.js
@@ -14,11 +14,9 @@ await registerAvaloniaModule(dotnetRuntime);
 const config = dotnetRuntime.getConfig();
 const exports = await dotnetRuntime.getAssemblyExports(config.mainAssemblyName);
 
-exports.ChatGPT.UI.Browser.Services.Interop.SaveSettings();
-
 window.addEventListener('beforeunload',  (event) => {
     console.log("[JS] Saving settings...");
-    exports.Interop.SaveSettings();
+    exports.ChatGPT.UI.Browser.Services.Interop.SaveSettings();
     console.log("[JS] Saved settings.");
     event.preventDefault();
     event.returnValue = true;
@@ -28,7 +26,7 @@ const terminationEvent = 'onpagehide' in self ? 'pagehide' : 'unload';
 
 window.addEventListener(terminationEvent,  (event) => {
     console.log("[JS] Saving settings...");
-    exports.Interop.SaveSettings();
+    exports.ChatGPT.UI.Browser.Services.Interop.SaveSettings();
     console.log("[JS] Saved settings.");
 });
 
