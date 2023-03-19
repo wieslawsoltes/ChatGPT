e0be489ac41cc9fcf59dac77bf983ec4e491c08b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-19 21:11:53 +0100

    Update MainViewModel.cs

--- ChatGPT/ViewModels/MainViewModel.cs
+++ ChatGPT/ViewModels/MainViewModel.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
