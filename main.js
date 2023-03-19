6d2337ef65615bbd2ad2351d72338a704662773a
wieslawsoltes <wieslawsoltes@users.noreply.github.com>
2023-03-19 20:20:12 +0000

    Deploying to gh-pages from @ wieslawsoltes/ChatGPT@478fa642601c54aea8dadc6cb69eedd82fbdb8a7 🚀

--- main.js
+++ main.js
@@ -12,5 +12,16 @@ const dotnetRuntime = await dotnet
 await registerAvaloniaModule(dotnetRuntime);
 
 const config = dotnetRuntime.getConfig();
+const exports = await dotnetRuntime.getAssemblyExports(config.mainAssemblyName);
+
+window.addEventListener('beforeunload', (event) => {
+    exports.ChatGPT.UI.Browser.Services.Interop.SaveSettings();
+});
+
+const terminationEvent = 'onpagehide' in self ? 'pagehide' : 'unload';
+
+window.addEventListener(terminationEvent, (event) => {
+    exports.ChatGPT.UI.Browser.Services.Interop.SaveSettings();
+});
 
 await dotnetRuntime.runMainAndExit(config.mainAssemblyName, [window.location.search]);
