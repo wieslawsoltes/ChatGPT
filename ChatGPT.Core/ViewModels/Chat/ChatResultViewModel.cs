30ec2ab9c7961e9bed0fa1e73cf9d9f163651c4b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-21 13:17:53 +0100

    Create ChatResultViewModel.cs

---
+++ ChatGPT.Core/ViewModels/Chat/ChatResultViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ChatGPT.ViewModels.Chat;
+
+public class ChatResultViewModel : ObservableObject
+{
+    private string? _message;
+    private bool _isError;
+
+    [JsonPropertyName("name")]
+    public string? Message
+    {
+        get => _message;
+        set => SetProperty(ref _message, value);
+    }
+
+    [JsonPropertyName("isError")]
+    public bool IsError
+    {
+        get => _isError;
+        set => SetProperty(ref _isError, value);
+    }
+}
