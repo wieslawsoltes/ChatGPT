a15832257263a844cfcc5c7f8d74e0a6be187ebd
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-29 11:27:49 +0200

    Update Program.cs

--- ChatGPT.CLI.Chaining/Program.cs
+++ ChatGPT.CLI.Chaining/Program.cs
@@ -3,32 +3,24 @@ using ChatGPT.ViewModels.Chat;
 
 Defaults.ConfigureDefaultServices();
 
+using var cts = new CancellationTokenSource();
 var input1 = "You are a helpful assistant.";
 var directions1 = "You are a helpful assistant. Convert user message into hex string. Write only the output hex string. Do not write any additional text.";
-var input2 = await SendAsync(directions1, input1);
+var input2 = await SendAsync(directions1, input1, cts.Token);
 Console.WriteLine(input2);
 if (input2 is not null)
 {
     var directions2 = "You are a helpful assistant. Convert user hex string into plain text. Write only the output plain text. Do not write any additional text.";
-    var output = await SendAsync(directions2, input2);
+    var output = await SendAsync(directions2, input2, cts.Token);
     Console.WriteLine(output);
 }
 
-async Task<string?> SendAsync(string directions, string input)
+async Task<string?> SendAsync(string directions, string input, CancellationToken token)
 {
-    var chat = new ChatViewModel
-    {
-        Settings = new ChatSettingsViewModel
-        {
-            MaxTokens = 2000,
-            Model = "gpt-3.5-turbo",
-            Directions = directions
-        }
-    };
+    var chat = new ChatViewModel();
     chat.AddSystemMessage(directions);
     chat.AddUserMessage(input);
-    using var cts = new CancellationTokenSource();
-    var result = await chat.SendAsync(chat.CreateChatMessages(), cts.Token);
+    var result = await chat.SendAsync(chat.CreateChatMessages(), token);
     chat.AddAssistantMessage(result?.Message);
     return result?.Message;
 }
