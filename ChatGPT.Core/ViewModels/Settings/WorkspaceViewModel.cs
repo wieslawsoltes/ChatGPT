f90f18ddd85776625c6688965e671039788b8638
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-29 12:05:40 +0200

    Update WorkspaceViewModel.cs

--- ChatGPT.Core/ViewModels/Settings/WorkspaceViewModel.cs
+++ ChatGPT.Core/ViewModels/Settings/WorkspaceViewModel.cs
@@ -8,18 +8,34 @@ namespace ChatGPT.ViewModels.Settings;
 
 public class WorkspaceViewModel : ObservableObject
 {
-    private ObservableCollection<ChatViewModel>? _chats;
+    private string? _name;
+    private ObservableCollection<ChatViewModel> _chats;
     private ChatViewModel? _currentChat;
-    private ObservableCollection<PromptViewModel>? _prompts;
+    private ObservableCollection<PromptViewModel> _prompts;
     private PromptViewModel? _currentPrompt;
-    private ObservableCollection<LayoutViewModel>? _layouts;
+    private ObservableCollection<LayoutViewModel> _layouts;
     private LayoutViewModel? _currentLayout;
     private string? _theme;
     private string? _layout;
     private bool _topmost;
 
+    [JsonConstructor]
+    public WorkspaceViewModel()
+    {
+        _chats = new ObservableCollection<ChatViewModel>();
+        _prompts = new ObservableCollection<PromptViewModel>();
+        _layouts = new ObservableCollection<LayoutViewModel>();
+    }
+
+    [JsonPropertyName("name")]
+    public string? Name
+    {
+        get => _name;
+        set => SetProperty(ref _name, value);
+    }
+
     [JsonPropertyName("chats")]
-    public ObservableCollection<ChatViewModel>? Chats
+    public ObservableCollection<ChatViewModel> Chats
     {
         get => _chats;
         set => SetProperty(ref _chats, value);
@@ -33,7 +49,7 @@ public class WorkspaceViewModel : ObservableObject
     }
     
     [JsonPropertyName("prompts")]
-    public ObservableCollection<PromptViewModel>? Prompts
+    public ObservableCollection<PromptViewModel> Prompts
     {
         get => _prompts;
         set => SetProperty(ref _prompts, value);
@@ -47,7 +63,7 @@ public class WorkspaceViewModel : ObservableObject
     }
 
     [JsonPropertyName("layouts")]
-    public ObservableCollection<LayoutViewModel>? Layouts
+    public ObservableCollection<LayoutViewModel> Layouts
     {
         get => _layouts;
         set => SetProperty(ref _layouts, value);
@@ -79,5 +95,85 @@ public class WorkspaceViewModel : ObservableObject
     {
         get => _topmost;
         set => SetProperty(ref _topmost, value);
+    }
+
+    private ObservableCollection<ChatViewModel> CopyChats(out ChatViewModel? currentChat)
+    {
+        var chats = new ObservableCollection<ChatViewModel>();
+
+        currentChat = null;
+
+        foreach (var chat in _chats)
+        {
+            var chatCopy = chat.Copy();
+
+            chats.Add(chatCopy);
+
+            if (chat == _currentChat)
+            {
+                currentChat = chatCopy;
+            }
+        }
+
+        return chats;
+    }
+
+    private ObservableCollection<PromptViewModel> CopyPrompts(out PromptViewModel? currentPrompt)
+    {
+        var prompts = new ObservableCollection<PromptViewModel>();
+
+        currentPrompt = null;
+
+        foreach (var prompt in _prompts)
+        {
+            var promptCopy = prompt.Copy();
+
+            prompts.Add(promptCopy);
+
+            if (prompt == _currentPrompt)
+            {
+                currentPrompt = promptCopy;
+            }
+        }
+
+        return prompts;
+    }
+
+    private ObservableCollection<LayoutViewModel> CopyLayouts(out LayoutViewModel? currentLayout)
+    {
+        var layouts = new ObservableCollection<LayoutViewModel>();
+
+        currentLayout = null;
+
+        foreach (var layout in _layouts)
+        {
+            var layoutCopy = layout.Copy();
+
+            layouts.Add(layoutCopy);
+
+            if (layout == _currentLayout)
+            {
+                currentLayout = layoutCopy;
+            }
+        }
+
+        return layouts;
+    }
+
+    public WorkspaceViewModel Copy()
+    {
+        return new WorkspaceViewModel
+        {
+            Name = _name,
+            Chats = CopyChats(out var currentChat),
+            CurrentChat = currentChat,
+            Prompts = CopyPrompts(out var currentPrompt),
+            CurrentPrompt = currentPrompt,
+            Layouts = CopyLayouts(out var currentLayout),
+            CurrentLayout = currentLayout,
+            Theme = _theme,
+            Layout = _layout,
+            Topmost = _topmost,
+        };
     }
 }
