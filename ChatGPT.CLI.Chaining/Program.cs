5d73ca498a12c107d15e42681f7116fd7d0dd535
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-29 12:04:58 +0200

    Update Program.cs

--- ChatGPT.CLI.Chaining/Program.cs
+++ ChatGPT.CLI.Chaining/Program.cs
@@ -4,7 +4,7 @@ using ChatGPT.ViewModels.Chat;
 Defaults.ConfigureDefaultServices();
 
 using var cts = new CancellationTokenSource();
-var input1 = "You are a helpful assistant.";
+var input1 = "Some random test string.";
 var directions1 = "You are a helpful assistant. Convert user message into hex string. Write only the output hex string. Do not write any additional text.";
 var input2 = await SendAsync(directions1, input1, cts.Token);
 Console.WriteLine(input2);
@@ -17,7 +17,7 @@ if (input2 is not null)
 
 async Task<string?> SendAsync(string directions, string input, CancellationToken token)
 {
-    var chat = new ChatViewModel();
+    var chat = new ChatViewModel(directions);
     chat.AddSystemMessage(directions);
     chat.AddUserMessage(input);
     var result = await chat.SendAsync(chat.CreateChatMessages(), token);
