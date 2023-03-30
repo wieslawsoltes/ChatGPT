84b2d422d9b76eb5ea07ec690575509351520bdb
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-03-30 10:41:01 +0200

    Update LayoutContainer.cs

--- ChatGPT.UI/Controls/LayoutContainer.cs
+++ ChatGPT.UI/Controls/LayoutContainer.cs
@@ -1,6 +1,5 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.VisualTree;
 using ChatGPT.Views.Layouts;
 
 namespace ChatGPT.Controls;
@@ -10,33 +9,20 @@ public class LayoutContainer : Decorator
     public static readonly StyledProperty<string?> LayoutProperty = 
         AvaloniaProperty.Register<LayoutContainer, string?>(nameof(Layout));
 
-    public static readonly StyledProperty<double> LayoutWidthProperty = 
-        AvaloniaProperty.Register<LayoutContainer, double>(nameof(LayoutWidth));
-
-    public static readonly StyledProperty<double> LayoutHeightProperty = 
-        AvaloniaProperty.Register<LayoutContainer, double>(nameof(LayoutHeight));
-
     public string? Layout
     {
         get => GetValue(LayoutProperty);
         set => SetValue(LayoutProperty, value);
     }
 
-    public double LayoutWidth
+    private readonly SingleLayoutView _single;
+    private readonly ColumnLayoutView _column;
+
+    public LayoutContainer()
     {
-        get => GetValue(LayoutWidthProperty);
-        set => SetValue(LayoutWidthProperty, value);
+        _single = new SingleLayoutView();
+        _column = new ColumnLayoutView();  
     }
-
-    public double LayoutHeight
-    {
-        get => GetValue(LayoutHeightProperty);
-        set => SetValue(LayoutHeightProperty, value);
-    }
-
-    private SingleLayoutView Single = new SingleLayoutView();
-
-    private ColumnLayoutView Column = new ColumnLayoutView();
 
     protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
     {
@@ -49,34 +35,14 @@ public class LayoutContainer : Decorator
             {
                 case "Mobile":
                 {
-                    Child = Single;
+                    Child = _single;
                     break;
                 }
                 case "Desktop":
                 {
-                    Child =  Column;
+                    Child = _column;
                     break;
                 }
-            }
-        }
-
-        if (change.Property == LayoutWidthProperty)
-        {
-            var width = change.GetNewValue<double>();
-            if (this.GetVisualRoot() is TopLevel topLevel)
-            {
-                // TODO:
-                // topLevel.Width = width;
-            }
-        }
-
-        if (change.Property == LayoutHeightProperty)
-        {
-            var height = change.GetNewValue<double>();
-            if (this.GetVisualRoot() is TopLevel topLevel)
-            {
-                // TODO:
-                // topLevel.Height = height;
             }
         }
     }
