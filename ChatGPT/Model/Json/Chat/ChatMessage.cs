7dde66f5b1a6b2e6640b7445916075c05d8dcae4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-04-02 13:52:58 +0200

    Annotate with data contracts attributes

--- ChatGPT/Model/Json/Chat/ChatMessage.cs
+++ ChatGPT/Model/Json/Chat/ChatMessage.cs
@@ -1,12 +1,16 @@
+using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 
 namespace AI.Model.Json.Chat;
 
+[DataContract]
 public class ChatMessage
 {
+    [DataMember(Name = "role")]
     [JsonPropertyName("role")]
     public string? Role { get; set; }
 
+    [DataMember(Name = "content")]
     [JsonPropertyName("content")]
     public string? Content { get; set; }
 }
--- ChatGPT/Model/Json/Chat/ChatRequestBody.cs
+++ ChatGPT/Model/Json/Chat/ChatRequestBody.cs
@@ -1,43 +1,57 @@
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 
 namespace AI.Model.Json.Chat;
 
+[DataContract]
 public class ChatRequestBody
 {
+    [DataMember(Name = "model")]
     [JsonPropertyName("model")]
     public string? Model { get; set; }
 
+    [DataMember(Name = "messages")]
     [JsonPropertyName("messages")]
     public ChatMessage[]? Messages { get; set; }
 
+    [DataMember(Name = "temperature")]
     [JsonPropertyName("temperature")]
     public decimal Temperature { get; set; } = 1;
 
+    [DataMember(Name = "top_p")]
     [JsonPropertyName("top_p")]
     public decimal TopP { get; set; } = 1;
 
+    [DataMember(Name = "n")]
     [JsonPropertyName("n")]
     public int N { get; set; } = 1;
 
+    [DataMember(Name = "stream")]
     [JsonPropertyName("stream")]
     public bool Stream { get; set; }
 
+    [DataMember(Name = "stop")]
     [JsonPropertyName("stop")]
     public string? Stop { get; set; }
 
+    [DataMember(Name = "max_tokens")]
     [JsonPropertyName("max_tokens")]
     public int MaxTokens { get; set; } = 16;
 
+    [DataMember(Name = "presence_penalty")]
     [JsonPropertyName("presence_penalty")]
     public decimal PresencePenalty { get; set; }
 
+    [DataMember(Name = "frequency_penalty")]
     [JsonPropertyName("frequency_penalty")]
     public decimal FrequencyPenalty { get; set; }
 
+    [DataMember(Name = "logit_bias")]
     [JsonPropertyName("logit_bias")]
     public Dictionary<string, decimal>? LogitBias { get; set; }
 
+    [DataMember(Name = "user")]
     [JsonPropertyName("user")]
     public string? User { get; set; }
 }
