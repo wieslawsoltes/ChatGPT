e9ce9dc551c2c74de1c95b01ccace504115337c8
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-04-07 19:11:54 +0200

    Update main.js

--- src/ChatGPT.UI.Browser/AppBundle/main.js
+++ src/ChatGPT.UI.Browser/AppBundle/main.js
@@ -1,5 +1,4 @@
 import { dotnet } from './dotnet.js'
-import { registerAvaloniaModule } from './avalonia.js';
 
 const is_browser = typeof window != "undefined";
 if (!is_browser) throw new Error(`Expected to be running in a browser`);
@@ -8,8 +7,6 @@ const dotnetRuntime = await dotnet
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
     .create();
-
-await registerAvaloniaModule(dotnetRuntime);
 
 const config = dotnetRuntime.getConfig();
 const exports = await dotnetRuntime.getAssemblyExports(config.mainAssemblyName);
