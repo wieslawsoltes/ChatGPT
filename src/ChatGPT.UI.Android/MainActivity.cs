71c6d1f9e6a3898496a991e4a43d30a85f540b83
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-04-08 17:43:18 +0200

    Update MainActivity.cs

--- src/ChatGPT.UI.Android/MainActivity.cs
+++ src/ChatGPT.UI.Android/MainActivity.cs
@@ -5,7 +5,12 @@ using Avalonia.Android;
 
 namespace ChatGPT.UI.Android;
 
-[Activity(Label = "ChatGPT", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+[Activity(
+    Label = "ChatGPT", 
+    Theme = "@style/MyTheme.NoActionBar", 
+    Icon = "@drawable/icon", 
+    LaunchMode = LaunchMode.SingleInstance, 
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
 public class MainActivity : AvaloniaMainActivity
 {
     protected override void OnDestroy()
