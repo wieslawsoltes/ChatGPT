b2447d3cacd3b941f5edfb287b2db08fb26f8890
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-04-08 19:39:29 +0200

    Update ChatSettingsView.axaml.cs

--- src/ChatGPT.UI/Views/Chat/ChatSettingsView.axaml.cs
+++ src/ChatGPT.UI/Views/Chat/ChatSettingsView.axaml.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Reactive;
 
 namespace ChatGPT.Views.Chat;
 
@@ -7,5 +11,19 @@ public partial class ChatSettingsView : UserControl
     public ChatSettingsView()
     {
         InitializeComponent();
+
+        this.GetObservable(BoundsProperty)
+            .Subscribe(new AnonymousObserver<Rect>(_ =>
+            {
+                if (SettingsSelectingItemsControl.Items is ItemCollection itemCollection)
+                {
+                    var focused = itemCollection.FirstOrDefault(x => x is Control {IsKeyboardFocusWithin: true});
+
+                    if (focused is Control control)
+                    {
+                        control.BringIntoView();
+                    }
+                }
+            }));
     }
 }
