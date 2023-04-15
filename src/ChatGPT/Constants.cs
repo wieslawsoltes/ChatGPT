392d520add89c7eb98d6e3f76bf1f522c66fa22b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-04-15 23:08:29 +0200

    Update Constants.cs

--- src/ChatGPT/Constants.cs
+++ src/ChatGPT/Constants.cs
@@ -3,4 +3,6 @@ namespace AI;
 public static class Constants
 {
     public const string EnvironmentVariableApiKey = "OPENAI_API_KEY";
+    
+    public const string EnvironmentVariableApiModel = "OPENAI_API_MODEL";
 }
