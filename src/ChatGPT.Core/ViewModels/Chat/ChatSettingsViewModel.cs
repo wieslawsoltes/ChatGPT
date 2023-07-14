13826e865395f23d9df6e26499deaa3f4f6c5858
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-07-14 11:16:11 +0200

    Update ChatSettingsViewModel.cs

--- src/ChatGPT.Core/ViewModels/Chat/ChatSettingsViewModel.cs
+++ src/ChatGPT.Core/ViewModels/Chat/ChatSettingsViewModel.cs
@@ -30,6 +30,12 @@ public partial class ChatSettingsViewModel : ObservableObject
         _apiUrl = null;
     }
 
+    /// <summary>
+    /// What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+    /// </summary>
+    /// <remarks>
+    /// We generally recommend altering this or top_p but not both.
+    /// </remarks>
     [JsonPropertyName("temperature")]
     public decimal Temperature
     {
@@ -37,6 +43,12 @@ public partial class ChatSettingsViewModel : ObservableObject
         set => SetProperty(ref _temperature, value);
     }
 
+    /// <summary>
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    /// </summary>
+    /// <remarks>
+    /// We generally recommend altering this or temperature but not both.
+    /// </remarks>
     [JsonPropertyName("top_p")]
     public decimal TopP
     {
@@ -44,6 +56,9 @@ public partial class ChatSettingsViewModel : ObservableObject
         set => SetProperty(ref _topP, value);
     }
 
+    /// <summary>
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    /// </summary>
     [JsonPropertyName("presence_penalty")]
     public decimal PresencePenalty
     {
@@ -51,6 +66,9 @@ public partial class ChatSettingsViewModel : ObservableObject
         set => SetProperty(ref _presencePenalty, value);
     }
 
+    /// <summary>
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    /// </summary>
     [JsonPropertyName("frequency_penalty")]
     public decimal FrequencyPenalty
     {
