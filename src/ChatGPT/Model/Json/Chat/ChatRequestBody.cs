7bf3b97b3eef62cbd7b58d95029cfbd6a7398813
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-07-14 12:58:48 +0200

    Update ChatRequestBody.cs

--- src/ChatGPT/Model/Json/Chat/ChatRequestBody.cs
+++ src/ChatGPT/Model/Json/Chat/ChatRequestBody.cs
@@ -34,8 +34,9 @@ public class ChatRequestBody
     [JsonPropertyName("functions")]
     public ChatFunction[]? Functions { get; set; }
 
-    // string: "none", "auto"
-    // ChatFunctionCall: {"name":\ "my_function"}
+    /// <summary>
+    /// Controls how the model responds to function calls. "none" means the model does not call a function, and responds to the end-user. "auto" means the model can pick between an end-user or calling a function. Specifying a particular function via {"name":\ "my_function"} forces the model to call that function. "none" is the default when no functions are present. "auto" is the default if functions are present.
+    /// </summary>
     [DataMember(Name = "function_call")]
     [JsonPropertyName("function_call")]
     public object? FunctionCall { get; set; }
