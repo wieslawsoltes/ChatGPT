113ef50d0b244edab697f13887c1718268c683ee
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-07-14 12:58:51 +0200

    Update ChatServiceSettings.cs

--- src/ChatGPT/Model/Services/ChatServiceSettings.cs
+++ src/ChatGPT/Model/Services/ChatServiceSettings.cs
@@ -7,6 +7,8 @@ public class ChatServiceSettings
     public string? ApiUrl { get; set; }
     public string? Model { get; set; }
     public ChatMessage[]? Messages { get; set; }
+    public ChatFunction[]? Functions { get; set; }
+    public object? FunctionCall { get; set; }
     public string? Suffix { get; set; }
     public decimal Temperature { get; set; }
     public int MaxTokens { get; set; }
