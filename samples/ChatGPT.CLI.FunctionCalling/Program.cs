7ed4fce5594447f0323e9140021deffab996188b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2023-07-23 14:54:34 +0200

    Add ChatGPT.CLI.FunctionCalling sample

---
+++ samples/ChatGPT.CLI.FunctionCalling/Program.cs
@@ -0,0 +1,103 @@
+﻿using ChatGPT;
+using ChatGPT.ViewModels.Chat;
+
+Defaults.ConfigureDefaultServices();
+
+var directions = 
+"""
+You are a helpful assistant.
+Write answers in plain text.
+Do not use markdown.
+""";
+
+if (args.Length == 1)
+{
+    directions = args[0];
+}
+
+using var cts = new CancellationTokenSource();
+
+var functions = new[]
+{
+    new
+    {
+        name = "get_current_weather",
+        description = "Get the current weather in a given location",
+        parameters = new
+        {
+            type = "object",
+            properties = new
+            {
+                location = new
+                {
+                    type = "string",
+                    description = "The city and state, e.g. San Francisco, CA"
+                },
+                unit = new
+                {
+                    type = "string",
+                    @enum = new[] { "celsius", "fahrenheit" }
+                },
+            },
+            required = new[] { "location" }
+        },
+    }
+};
+
+var chat = new ChatViewModel(new ChatSettingsViewModel
+{
+    MaxTokens = 2000,
+    Model = "gpt-3.5-turbo-0613",
+    Functions = functions,
+    FunctionCall = "auto"
+    // Force function call by setting FunctionCall property.
+    // FunctionCall = new { name = "get_current_weather" }
+});
+
+chat.AddSystemMessage(directions);
+
+while (true)
+{
+    Console.Write("> ");
+
+    var input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input) || input == Environment.NewLine)
+    {
+        continue;
+    }
+
+    try
+    {
+        chat.AddUserMessage(input);
+        var result = await chat.SendAsync(chat.CreateChatMessages(), cts.Token);
+
+        chat.AddAssistantMessage(result?.Message);
+
+        if (result?.Message is { })
+        {
+            Console.WriteLine(result.Message);
+        }
+
+        if (result?.FunctionCall is { } functionCall)
+        {
+            if (functionCall.Name == "get_current_weather" && functionCall.Arguments is { })
+            {
+                functionCall.Arguments.TryGetValue("location", out var location);
+                functionCall.Arguments.TryGetValue("location", out var unit);
+                var functionCallResult = get_current_weather(location, unit ?? "celsius");
+                chat.AddFunctionMessage(functionCallResult, functionCall.Name);
+
+                Console.WriteLine(functionCallResult);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Error: " + ex.Message);
+    }
+}
+
+string get_current_weather(string? location, string? unit)
+{
+    return "Cloudy.";
+}
